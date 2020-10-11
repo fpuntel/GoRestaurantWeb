187b23e36ef319344fd4cf678a23dc1afeb50401
Fernando <fernandopuntel@gmail.com>
2020-10-10 22:46:12 -0300

    goRestaurantWeb

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -4,6 +4,8 @@ import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 
+import api from '../../services/api';
+
 interface IFoodPlate {
   id: number;
   name: string;
@@ -27,6 +29,10 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+    });
     setIsAvailable(!isAvailable);
   }
 
